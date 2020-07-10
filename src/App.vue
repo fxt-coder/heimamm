4384c673035aca35adc2134f8fc7fb4747240cdf
傅雪涛 <1403664294@qq.com>
2020-07-10 09:40:14 +0800

    项目起步 - Element-ui整合

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <el-button @click="click">默认按钮</el-button>
+  </div>
+</template>
+<script>
+export default {
+  methods:{
+    click() {
+      alert('点了我一下');
+    }
+  }
+};
+</script>
+<style>
+</style>
