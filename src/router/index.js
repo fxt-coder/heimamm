dcf9d13f84f17ca5e4b4da899bb1186951744566
傅雪涛 <1403664294@qq.com>
2020-07-10 10:25:02 +0800

    无

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,25 @@
 /*
-
-
+1安装
+2导入
+3.注册
+4实例化
+5注入到new Vue({})
+6路由出口
 */
+//2导入
 import VueRouter from 'vue-router';
+// 3注册
 import Vue from 'vue'
 Vue.use(VueRouter)
-
+// 4实例化
 const router = new VueRouter({
   routes:[]
 })
+// 1.
 // export{router}
+//export {名字} 与 import {名字} 两者要对应起来
+//2 
+// export default 名字 与 import 名字 from 路径
+// 5.注入new Vue ({})
+// 6.路由出口
 export default router
